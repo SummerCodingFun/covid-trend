307fc4cc04ccfe6b4150f98d7ffc59d0b29112bf
Tom Wong <tom.pc.wong@gmail.com>
2020-08-07 01:13:06 -0700

    Updated scripts for RESTool

--- copy-to-remote.sh
+++ copy-to-remote.sh
@@ -1,5 +1,7 @@
 scp -i ~/Documents/AWS/LightsailDefaultKey-us-west-2.pem ./build/distributions/covid-trend.zip ubuntu@44.226.33.29:.
 scp -i ~/Documents/AWS/LightsailDefaultKey-us-west-2.pem ~/src/covid-trend/covid-cases-prod.yml ubuntu@44.226.33.29:.
+scp -i ~/Documents/AWS/LightsailDefaultKey-us-west-2.pem ~/src/covid-trend/setup-remote.sh ubuntu@44.226.33.29:.
 scp -i ~/Documents/AWS/LightsailDefaultKey-us-west-2.pem ~/src/covid-trend/run-on-remote.sh ubuntu@44.226.33.29:.
+scp -i ~/Documents/AWS/LightsailDefaultKey-us-west-2.pem ~/src/covid-trend/run-on-remote-RESTool.sh ubuntu@44.226.33.29:.
 scp -i ~/Documents/AWS/LightsailDefaultKey-us-west-2.pem ~/src/covid-trend/src/main/java/io/summercodingfun/covidtrend/resources/covidDB.sql ubuntu@44.226.33.29:.
-scp -i ~/Documents/AWS/LightsailDefaultKey-us-west-2.pem ~/src/covid-trend/src/main/java/io/summercodingfun/covidtrend/resources/us-states.csv ubuntu@44.226.33.29:.
+scp -i ~/Documents/AWS/LightsailDefaultKey-us-west-2.pem -r ~/src/covid-trend/RESTool2/build ubuntu@44.226.33.29:./RESTool2
--- setup-remote.sh
+++ setup-remote.sh
@@ -1,4 +1,5 @@
 mkdir -m 777 -p /opt/apps/logs
 sudo apt install unzip
-sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080
-sudo iptables -t nat -I OUTPUT -p tcp -o lo --dport 80 -j REDIRECT --to-ports 8080
+sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 5000
+sudo iptables -t nat -I OUTPUT -p tcp -o lo --dport 80 -j REDIRECT --to-ports 5000
+sudo npm install -g serve
