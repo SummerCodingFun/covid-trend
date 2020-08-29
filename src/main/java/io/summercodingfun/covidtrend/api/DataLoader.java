a34d54e464ab52c9ef6939a4c1c538f9fa017da4
tom <tom.pc.wong@gmail.com>
2020-08-29 14:44:18 -0700

    Fixed problem loading the loadData.sql script from jar

--- src/main/java/io/summercodingfun/covidtrend/api/DataLoader.java
+++ src/main/java/io/summercodingfun/covidtrend/api/DataLoader.java
@@ -4,6 +4,7 @@ import io.summercodingfun.covidtrend.resources.ConnectionPool;
 import org.apache.ibatis.jdbc.ScriptRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.yaml.snakeyaml.reader.StreamReader;
 
 import java.io.*;
 import java.net.URL;
@@ -22,27 +23,38 @@ public class DataLoader {
     }
 
     public void getData() {
-        logger.info("Retrieving New Data...");
-        InputStream inputStream = null;
-        try {
-            inputStream = new URL("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv").openStream();
-            Files.copy(inputStream, Paths.get("/tmp/us-states.csv"), StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
-            logger.error("IO Exception when copying data", e);
-        }
-        Connection conn = null;
-        logger.info("data retrieved");
+        logger.info("Retrieving new Data...");
 
+        try {
+            InputStream inputStream = new URL("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv").openStream();
+            Files.copy(inputStream, Paths.get("/tmp/us-states.csv"), StandardCopyOption.REPLACE_EXISTING);
+            logger.info("New data retrieved");
+        } catch (IOException e) {
+            logger.error("IO Exception when copying new data", e);
+            return;
+        } catch (Exception e) {
+            logger.error("Unknown error retrieving new data", e);
+        }
+
+        Connection conn = null;
         try {
             conn = pool.getConnection();
             ScriptRunner sr = new ScriptRunner(conn);
-            File file = new File(getClass().getClassLoader().getResource("loadData.sql").getFile());
-            Reader reader = new FileReader(file);
-            sr.runScript(reader);
+            InputStream resource = getClass().getClassLoader().getResourceAsStream("loadData.sql");
+
+            if (resource == null) {
+                logger.error("Cannot find loadData script");
+                return;
+            } else {
+                logger.info("Found loadData script: {}", resource);
+                Reader reader = new InputStreamReader(resource);
+                sr.runScript(reader);
+                logger.info("loaded data successfully");
+            }
         } catch (SQLException s) {
             logger.error("SQL Exception when writing in data", s);
-        } catch (FileNotFoundException f) {
-            logger.error("File not found", f);
+        } catch (Exception e) {
+            logger.error("Unknown error running loadData", e);
         }
         finally {
             if (conn != null) {
@@ -53,6 +65,5 @@ public class DataLoader {
                 }
             }
         }
-        logger.info("loaded data successfully");
     }
 }
